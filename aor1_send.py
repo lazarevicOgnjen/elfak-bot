3721ae73e8d9767ad56d72fe905df287ba1851f0
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-06 18:32:08 +0100

    Update aor1_send.py

--- aor1_send.py
+++ aor1_send.py
@@ -42,51 +42,7 @@ if recipients:
             server.send_message(msg)
             print(f"✅ Sent to {to_email}")
 
-        import os, smtplib, time
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-
-SENDER_EMAIL = os.getenv("BOT_MAIL")
-SENDER_PASSWORD = os.getenv("BOT_PASS")
-SMTP_SERVER, SMTP_PORT = "smtp.gmail.com", 587
-RECEIVERS_FILE, IMAGE_PATH = "aip_emails.md", "aip.png"
-
-HTML_BODY = """
-<html><body>
-<p><img src="cid:embedded_image" style="width:300px;border-radius:10px;"></p>
-<p>AIP forum -> <a href="https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=3&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=">link</a></p>
-
-<p>Google form -> <a href="https://forms.gle/2XaMVYxLjiVikKCw5">link</a></p>
-</body></html>
-"""
-
-with open(RECEIVERS_FILE) as f:
-    recipients = [line.strip() for line in f if line.strip()]
-
-if recipients:
-    for to_email in recipients:
-        msg = MIMEMultipart("related")
-        msg["From"], msg["To"], msg["Subject"] = "Elfak bot", to_email, "🎓 AIP"
-        alt = MIMEMultipart("alternative")
-        msg.attach(alt)
-        alt.attach(MIMEText(HTML_BODY, "html"))
-
-        try:
-            with open(IMAGE_PATH, "rb") as img:
-                mime_img = MIMEImage(img.read())
-                mime_img.add_header("Content-ID", "<embedded_image>")
-                msg.attach(mime_img)
-        except FileNotFoundError:
-            pass
-
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(SENDER_EMAIL, SENDER_PASSWORD)
-            server.send_message(msg)
-            print(f"✅ Sent to {to_email}")
-
         time.sleep(1)
-
+        
 else:
     print("⚠️ No recipients found.")
