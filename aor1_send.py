296f53fd4c4e98d79a8c63c8df0b77c045b4844e
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-06 17:38:19 +0100

    Update aor1_send.py

--- aor1_send.py
+++ aor1_send.py
@@ -3,8 +3,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
-SENDER_EMAIL = os.getenv("AOR1_MAIL")
-SENDER_PASSWORD = os.getenv("AOR1_PASS")
+SENDER_EMAIL = os.getenv("BOT_MAIL")
+SENDER_PASSWORD = os.getenv("BOT_PASS")
 SMTP_SERVER, SMTP_PORT = "smtp.gmail.com", 587
 RECEIVERS_FILE, IMAGE_PATH = "aor1_emails.md", "aor1.png"
 
