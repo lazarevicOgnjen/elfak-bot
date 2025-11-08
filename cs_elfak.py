49f750e0651e222a789c65f2bf84eee05b42d247
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-08 23:18:32 +0100

    Update cs_elfak.py

--- cs_elfak.py
+++ cs_elfak.py
@@ -18,8 +18,16 @@ page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options
 try:
 
     # cs login
-    page_to_scrape.get(os.environ['SAJT'])
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/login/index.php")
     time.sleep(2)
+
+    element1 = page_to_scrape.find_element(By.XPATH, '//*[@id="details-button"]')
+    if element1:
+        print("website not safe")
+    
+    page_to_scrape.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[2]/div[3]/div/a').click()
+    time.sleep(2)
+    
     mail = page_to_scrape.find_element(By.XPATH, '//*[@id="i0116"]')
     mail.send_keys(os.environ['MAIL'])  
     page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
