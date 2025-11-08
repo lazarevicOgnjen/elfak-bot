80d194d077ea0556129a80e709a7144e1715b02f
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-08 22:51:30 +0100

    Update cs_elfak.py

--- cs_elfak.py
+++ cs_elfak.py
@@ -20,6 +20,13 @@ try:
     # cs login
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/login/index.php")
     time.sleep(2)
+
+    element1 = page_to_scrape.find_element(By.XPATH, "//*[@id="details-button"]")
+    if element1:
+        element1.click()
+        page_to_scrape.find_element(By.XPATH, "//*[@id="proceed-link"]")
+        time.sleep(2)
+    
     page_to_scrape.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[2]/div[3]/div/a").click()
     time.sleep(2)
     mail = page_to_scrape.find_element(By.XPATH, '//*[@id="i0116"]')
