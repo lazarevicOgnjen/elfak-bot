0a13b77222bad6128ed1a6d7297401bfe2960edd
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-11-16 00:04:56 +0100

    Update cs_elfak.py

--- cs_elfak.py
+++ cs_elfak.py
@@ -22,19 +22,19 @@ try:
     time.sleep(2)
     
  
-        page_to_scrape.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[2]/div[3]/div/a').click()
-        time.sleep(2)
+    page_to_scrape.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[2]/div[3]/div/a').click()
+    time.sleep(2)
     
-        mail = page_to_scrape.find_element(By.XPATH, '//*[@id="i0116"]')
-        mail.send_keys(os.environ['MAIL'])  
-        page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
-        time.sleep(2)   
-        password = page_to_scrape.find_element(By.XPATH, '//*[@id="i0118"]')
-        password.send_keys(os.environ['PASSWORD'])  
-        page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
-        time.sleep(2)
-        page_to_scrape.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
-        time.sleep(2)
+    mail = page_to_scrape.find_element(By.XPATH, '//*[@id="i0116"]')
+    mail.send_keys(os.environ['MAIL'])  
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
+    time.sleep(2)   
+    password = page_to_scrape.find_element(By.XPATH, '//*[@id="i0118"]')
+    password.send_keys(os.environ['PASSWORD'])  
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
+    time.sleep(2)
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
+    time.sleep(2)
     
         # bp
         page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
