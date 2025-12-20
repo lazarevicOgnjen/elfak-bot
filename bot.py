be397a2a8ffa5129be9ba8f003d5972db366d300
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-20 15:38:31 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
-from dhooks import Webhook, Embed, File
+from dhooks import Webhook
 
 WEBHOOK_URL = [os.getenv('bot')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
-    hook.send("@everyone sa strane imate opciju 'Channels & Roles', idete 'Channels & Roles' > 'Customise' tu imate drop-down u kome mozete izabrati predmete za koje zelite da dobijate obavestenja", embed=embed, file=file)
+    hook.send("@everyone sa strane imate opciju 'Channels & Roles', idete 'Channels & Roles' > 'Customise' tu imate drop-down u kome mozete izabrati predmete za koje zelite da dobijate obavestenja")
