e4bf001e19b1e00291a49af45ed3e0cef0a8ee38
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-20 13:43:53 +0100

    Create sip.py

---
+++ sip.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/google-chrome"
+
+browser_driver = Service('/usr/bin/chromedriver')
+
+page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
+
+try:
+    
+    page_to_scrape.get("https://sip.elfak.ni.ac.rs/")
+    time.sleep(3)
+
+    sipT = page_to_scrape.find_element(By.ID, 'novosti')
+    sip_markdown = sipT.text
+
+    with open("sip.md", "w") as sip_file:
+        sip_file.write(sip_markdown)
+
+    height = sipT.size['height']
+    width = sipT.size['width']
+
+    
+    desired_width = max(width, 1200)  
+
+    desired_height = min(height, 1000)
+
+    page_to_scrape.set_window_size(desired_width, desired_height)  
+
+   
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", sipT)
+
+    sipT.screenshot('sip.png')
+
+finally:
+    page_to_scrape.quit()
