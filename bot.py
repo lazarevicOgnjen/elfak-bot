f3d84dd55d3080d9a3673d101aada8a2fb0943b9
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-20 17:34:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,23 @@ from dhooks import Webhook
 WEBHOOK_URL = [os.getenv('bot')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
-    hook.send("@everyone sa strane imate opciju 'Channels & Roles', idete 'Channels & Roles' > 'Customise' tu imate drop-down u kome mozete izabrati predmete za koje zelite da dobijate obavestenja")
+    hook.send("@everyone I godina
+
+uur - uvod u racunarstvo
+aip - algoritmi i programiranje
+
+II godina RI
+
+bp - baze podataka
+oop - objektno orijentisano programiranje
+oopj - objektno orijentisano projektovanje
+pj - programski jezici
+dmat - diskretna matematika
+lp - logicko projektovanje
+sp - strukture podataka
+
+II godina
+
+os - operativno sistemi
+rm - racunarske mreze
+ti - teorija igara")
