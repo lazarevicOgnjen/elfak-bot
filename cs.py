828087a755427a54c347fb23fae2cb548b4c4609
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:45:53 +0100

    Update cs.py

--- cs.py
+++ cs.py
@@ -43,18 +43,18 @@ try:
     
     uurT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     uur_markdown = uurT.text
-    uurt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    uurT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("uur.md", "w") as uur_file:
         uur_file.write(uur_markdown)
 
-    heightUUR = uurt.size['height']
-    widthUUR = uurt.size['width']
+    heightUUR = uurT.size['height']
+    widthUUR = uurT.size['width']
     desired_widthUUR = max(widthUUR, 1200)  
     desired_heightUUR = min(heightUUR, 1000)
     page_to_scrape.set_window_size(desired_widthUUR, desired_heightUUR)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", uurt)
-    uurt.screenshot('uur.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", uurT)
+    uurT.screenshot('uur.png')
 
     # aip
 
@@ -63,18 +63,18 @@ try:
     
     aipT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     aip_markdown = aipT.text
-    aipt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    aipT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("aip.md", "w") as aip_file:
         aip_file.write(aip_markdown)
 
-    heightAIP = aipt.size['height']
-    widthAIP = aipt.size['width']
+    heightAIP = aipT.size['height']
+    widthAIP = aipT.size['width']
     desired_widthAIP = max(widthAIP, 1200)  
     desired_heightAIP = min(heightAIP, 1000)
     page_to_scrape.set_window_size(desired_widthAIP, desired_heightAIP)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", aipt)
-    aipt.screenshot('aip.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", aipT)
+    aipT.screenshot('aip.png')
 
     # II godina
     # bp
@@ -84,18 +84,18 @@ try:
     
     bpT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     bp_markdown = bpT.text
-    bpt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    bpT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("bp.md", "w") as bp_file:
         bp_file.write(bp_markdown)
 
-    heightBP = bpt.size['height']
-    widthBP = bpt.size['width']
+    heightBP = bpT.size['height']
+    widthBP = bpT.size['width']
     desired_widthBP = max(widthBP, 1200)  
     desired_heightBP = min(heightBP, 1000)
     page_to_scrape.set_window_size(desired_widthBP, desired_heightBP)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", bpt)
-    bpt.screenshot('bp.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", bpT)
+    bpT.screenshot('bp.png')
 
     # dmat
 
@@ -104,18 +104,18 @@ try:
     
     dmatT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     dmat_markdown = dmatT.text
-    dmatt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    dmatT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("dmat.md", "w") as dmat_file:
         dmat_file.write(dmat_markdown)
 
-    heightDMAT = dmatt.size['height']
-    widthDMAT = dmatt.size['width']
+    heightDMAT = dmatT.size['height']
+    widthDMAT = dmatT.size['width']
     desired_widthDMAT = max(widthDMAT, 1200)  
     desired_heightDMAT = min(heightDMAT, 1000)
     page_to_scrape.set_window_size(desired_widthDMAT, desired_heightDMAT)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", dmatt)
-    dmatt.screenshot('dmat.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", dmatT)
+    dmatT.screenshot('dmat.png')
 
     # oop
 
@@ -124,18 +124,18 @@ try:
     
     oopT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     oop_markdown = oopT.text
-    oopt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    oopT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("oop.md", "w") as oop_file:
         oop_file.write(oop_markdown)
 
-    heightOOP = oopt.size['height']
-    widthOOP = oopt.size['width']
+    heightOOP = oopT.size['height']
+    widthOOP = oopT.size['width']
     desired_widthOOP = max(widthOOP, 1200)  
     desired_heightOOP = min(heightOOP, 1000)
     page_to_scrape.set_window_size(desired_widthOOP, desired_heightOOP)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", oopt)
-    oopt.screenshot('oop.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", oopT)
+    oopT.screenshot('oop.png')
 
 
     # lp
@@ -145,18 +145,18 @@ try:
     
     lpT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     lp_markdown = lpT.text
-    lpt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    lpT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("lp.md", "w") as lp_file:
         lp_file.write(lp_markdown)
 
-    heightLP = lpt.size['height']
-    widthLP = lpt.size['width']
+    heightLP = lpT.size['height']
+    widthLP = lpT.size['width']
     desired_widthLP = max(widthLP, 1200)  
     desired_heightLP = min(heightLP, 1000)
     page_to_scrape.set_window_size(desired_widthLP, desired_heightLP)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", lpt)
-    lpt.screenshot('lp.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", lpT)
+    lpT.screenshot('lp.png')
 
 
 
@@ -168,18 +168,18 @@ try:
     
     oopjT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     oopj_markdown = oopjT.text
-    oopjt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    oopjT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("oopj.md", "w") as oopj_file:
         oopj_file.write(oopj_markdown)
 
-    heightOOPJ = oopjt.size['height']
-    widthOOPJ = oopjt.size['width']
+    heightOOPJ = oopjT.size['height']
+    widthOOPJ = oopjT.size['width']
     desired_widthOOPJ = max(widthOOPJ, 1200)  
     desired_heightOOPJ = min(heightOOPJ, 1000)
     page_to_scrape.set_window_size(desired_widthOOPJ, desired_heightOOPJ)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", oopjt)
-    oopjt.screenshot('oopj.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", oopjT)
+    oopjT.screenshot('oopj.png')
 
 
 
@@ -190,18 +190,18 @@ try:
     
     spT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     sp_markdown = spT.text
-    spt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    spT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("sp.md", "w") as sp_file:
         sp_file.write(sp_markdown)
 
-    heightSP = spt.size['height']
-    widthSP = spt.size['width']
+    heightSP = spT.size['height']
+    widthSP = spT.size['width']
     desired_widthSP = max(widthSP, 1200)  
     desired_heightSP = min(heightSP, 1000)
     page_to_scrape.set_window_size(desired_widthSP, desired_heightSP)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", spt)
-    spt.screenshot('sp.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", spT)
+    spT.screenshot('sp.png')
 
 
     # pj
@@ -211,18 +211,18 @@ try:
     
     pjT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     pj_markdown = pjT.text
-    pjt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    pjT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("pj.md", "w") as pj_file:
         pj_file.write(pj_markdown)
 
-    heightPJ = pjt.size['height']
-    widthPJ = pjt.size['width']
+    heightPJ = pjT.size['height']
+    widthPJ = pjT.size['width']
     desired_widthPJ = max(widthPJ, 1200)  
     desired_heightPJ = min(heightPJ, 1000)
     page_to_scrape.set_window_size(desired_widthPJ, desired_heightPJ)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", pjt)
-    pjt.screenshot('pj.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", pjT)
+    pjT.screenshot('pj.png')
 
     # III godina
     # os
@@ -232,18 +232,18 @@ try:
     
     osT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     os_markdown = osT.text
-    ost = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    osT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("os.md", "w") as os_file:
         os_file.write(os_markdown)
 
-    heightOS = ost.size['height']
-    widthOS = ost.size['width']
+    heightOS = osT.size['height']
+    widthOS = osT.size['width']
     desired_widthOS = max(widthOS, 1200)  
     desired_heightOS = min(heightOS, 1000)
     page_to_scrape.set_window_size(desired_widthOS, desired_heightOS)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", ost)
-    ost.screenshot('os.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", osT)
+    osT.screenshot('os.png')
 
 
     # rm
@@ -253,18 +253,18 @@ try:
     
     rmT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     rm_markdown = rmT.text
-    rmt = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    rmT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("rm.md", "w") as rm_file:
         rm_file.write(rm_markdown)
 
-    heightRM = rmt.size['height']
-    widthRM = rmt.size['width']
+    heightRM = rmT.size['height']
+    widthRM = rmT.size['width']
     desired_widthRM = max(widthRM, 1200)  
     desired_heightRM = min(heightRM, 1000)
     page_to_scrape.set_window_size(desired_widthRM, desired_heightRM)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", rmt)
-    rmt.screenshot('rm.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", rmT)
+    rmT.screenshot('rm.png')
 
     # ti
 
@@ -273,18 +273,18 @@ try:
     
     tiT = page_to_scrape.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/section/div/article[1]/div')
     ti_markdown = tiT.text
-    tit = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    tiT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
     with open("ti.md", "w") as ti_file:
         ti_file.write(ti_markdown)
 
-    heightTI = tit.size['height']
-    widthTI = tit.size['width']
+    heightTI = tiT.size['height']
+    widthTI = tiT.size['width']
     desired_widthTI = max(widthTI, 1200)  
     desired_heightTI = min(heightTI, 1000)
     page_to_scrape.set_window_size(desired_widthTI, desired_heightTI)    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", tit)
-    tit.screenshot('ti.png')
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", tiT)
+    tiT.screenshot('ti.png')
 
 
 
