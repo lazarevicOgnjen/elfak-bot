673feebfec56a41cced29dc278e534a00b7f5fc1
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:54:37 +0100

    Update oopDiscord.py

--- oopDiscord.py
+++ oopDiscord.py
@@ -9,7 +9,7 @@ hook = Webhook(WEBHOOK_URL)
 file = File(oop_image, name="oop.png")
 
 hook.send(
-    "@everyone OOP\n"
-    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    "@everyone OOP\n\n"
+    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)\n",
     file=file
 )
