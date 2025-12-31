6aea8ba2ca15fb0c0dcd9875bd4db589335f0918
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:12:34 +0100

    Update aipDiscord.py

--- aipDiscord.py
+++ aipDiscord.py
@@ -9,8 +9,8 @@ hook = Webhook(WEBHOOK_URL)
 file = File(aip_image, name="aip.png")
 
 hook.send(
-    "@everyone\n\n"
-    "**[website link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=3&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
+    "@everyone Algoritmi i programiranje\n"
+    "[cs link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=3&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
     file=file
 )
 
