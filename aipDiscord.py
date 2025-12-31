d48d93b34a0f2e618b0130ac8f1f2b003d3233b2
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:21:36 +0100

    Update aipDiscord.py

--- aipDiscord.py
+++ aipDiscord.py
@@ -10,7 +10,7 @@ file = File(aip_image, name="aip.png")
 
 hook.send(
     "@everyone AIP\n"
-    "[cs link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=3&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=3&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
     file=file
 )
 
