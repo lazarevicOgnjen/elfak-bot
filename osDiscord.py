19b2c857619e57b71acd08c5cb88e3017cc617c0
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 23:33:45 +0100

    Update osDiscord.py

--- osDiscord.py
+++ osDiscord.py
@@ -9,7 +9,7 @@ hook = Webhook(WEBHOOK_URL)
 file = File(os_image, name="os.png")
 
 hook.send(
-    "@everyone OS\n\n"
+    "@everyone  📢 OS\n\n"
     "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=55&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
     file=file
 )
