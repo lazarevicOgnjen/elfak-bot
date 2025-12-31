80b6f56d472decda9c12680d512f1caa233831a5
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 23:33:54 +0100

    Update pjDiscord.py

--- pjDiscord.py
+++ pjDiscord.py
@@ -9,7 +9,7 @@ hook = Webhook(WEBHOOK_URL)
 file = File(pj_image, name="pj.png")
 
 hook.send(
-    "@everyone PJ\n\n"
+    "@everyone  📢 PJ\n\n"
     "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=11&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
     file=file
 )
