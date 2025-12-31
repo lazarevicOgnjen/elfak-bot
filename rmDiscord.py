43a79f8cb6eafe0d0a403213ea1b53efeed2a205
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:27:14 +0100

    Update rmDiscord.py

--- rmDiscord.py
+++ rmDiscord.py
@@ -1,17 +1,15 @@
 import os
-from dhooks import Webhook, Embed, File
+from dhooks import Webhook, File
 
 rm_image = 'rm.png'
 
-WEBHOOK_URL = [os.getenv('rm')]
-for url in WEBHOOK_URL:
-    hook = Webhook(url)
+WEBHOOK_URL = os.getenv("rm")
+hook = Webhook(WEBHOOK_URL)
 
-    embed = Embed(
-        description="**[website link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=49&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
-        color=0x3498DB
-    )
-    
-    embed.set_image(url="attachment://rm.png")
-    file = File(rm_image, name="rm.png")
-    hook.send("@everyone", embed=embed, file=file)
+file = File(rm_image, name="rm.png")
+
+hook.send(
+    "@everyone RM\n"
+    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=49&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    file=file
+)
