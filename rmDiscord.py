6950bf91939f3bcd69a5d74b6ea06daea14a3d3b
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:55:37 +0100

    Update rmDiscord.py

--- rmDiscord.py
+++ rmDiscord.py
@@ -9,7 +9,7 @@ hook = Webhook(WEBHOOK_URL)
 file = File(rm_image, name="rm.png")
 
 hook.send(
-    "@everyone RM\n"
-    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=49&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    "@everyone RM\n\n"
+    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=49&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)\n",
     file=file
 )
