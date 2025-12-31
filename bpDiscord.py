ff9cb5b9dbd89211eb26ffe39b5f95e8c2329257
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:10:50 +0100

    Update bpDiscord.py

--- bpDiscord.py
+++ bpDiscord.py
@@ -1,17 +1,17 @@
 import os
-from dhooks import Webhook, Embed, File
+from dhooks import Webhook, File
 
 bp_image = 'bp.png'
 
-WEBHOOK_URL = [os.getenv('bp')]
-for url in WEBHOOK_URL:
-    hook = Webhook(url)
+WEBHOOK_URL = os.getenv("bp")
+hook = Webhook(WEBHOOK_URL)
 
-    embed = Embed(
-        description="**[website link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
-        color=0x3498DB
-    )
-    
-    embed.set_image(url="attachment://bp.png")
-    file = File(bp_image, name="bp.png")
-    hook.send("@everyone", embed=embed, file=file)
+file = File(bp_image, name="bp.png")
+
+hook.send(
+    "@everyone\n\n"
+    "Baze podataka"
+    "[website link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    file=file
+)
+
