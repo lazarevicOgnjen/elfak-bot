357d14b009bce1f60199251681536c52d0095831
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-12-31 22:53:40 +0100

    Update bpDiscord.py

--- bpDiscord.py
+++ bpDiscord.py
@@ -9,8 +9,8 @@ hook = Webhook(WEBHOOK_URL)
 file = File(bp_image, name="bp.png")
 
 hook.send(
-    "@everyone BP\n"
-    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)",
+    "@everyone BP\n\n"
+    "[- forum post link -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)\n",
     file=file
 )
 
