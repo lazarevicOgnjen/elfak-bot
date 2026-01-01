f0596599be16c9bcea3a4b625d4cdaeff8b24cfb
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-01 01:48:12 +0100

    Update sipDiscord.py

--- sipDiscord.py
+++ sipDiscord.py
@@ -1,7 +1,13 @@
 import os
-from dhooks import Webhook, File
+from dhooks import Webhook, File, Embed
 
 sip_image = 'sip.png'
+
+embed = Embed(
+    title="🔗 NEWS FORUM",
+    url="https://sip.elfak.ni.ac.rs",
+    color=0x2F3136
+)
 
 WEBHOOK_URL = os.getenv("sip")
 hook = Webhook(WEBHOOK_URL)
@@ -9,7 +15,7 @@ hook = Webhook(WEBHOOK_URL)
 file = File(sip_image, name="sip.png")
 
 hook.send(
-    "@everyone  📢 SIP\n\n"
-    "[- news forum link -](https://sip.elfak.ni.ac.rs)",
+    "@everyone  📢 SIP\n\n",
+    embed=embed,
     file=file
 )
