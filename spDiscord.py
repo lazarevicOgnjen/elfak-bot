fd1c8a016837cb56550ff18d4e06e9299d1f8389
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-03 00:19:14 +0100

    Update spDiscord.py

--- spDiscord.py
+++ spDiscord.py
@@ -10,7 +10,7 @@ class SPButton(discord.ui.View):
         super().__init__(timeout=None)
         self.add_item(
             discord.ui.Button(
-                label="SP forum link",
+                label="Forum link",
                 url="https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=9&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=",
                 style=discord.ButtonStyle.link
             )
