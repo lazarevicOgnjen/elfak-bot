8bbde45d67648371bf0e4def14dea62bcbfa9bea
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-03 17:40:33 +0100

    Update sipDiscord.py

--- sipDiscord.py
+++ sipDiscord.py
@@ -3,6 +3,32 @@ import os
 import asyncio
 import aiohttp
 
+# delete old message
+
+TOKEN = os.getenv("elfak_bot")
+CHANNEL_ID = os.getenv("sip_id")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+
+    deleted = 0
+    async for message in channel.history(limit=None):
+        await message.delete()
+        deleted += 1
+
+    print(f"Deleted {deleted} messages")
+    await client.close()
+
+client.run(TOKEN)
+
+# send new message
+
 WEBHOOK_URL = os.getenv("sip")
 
 class BOTButton(discord.ui.View):
@@ -30,3 +56,4 @@ async def main():
         )
 
 asyncio.run(main())
+
