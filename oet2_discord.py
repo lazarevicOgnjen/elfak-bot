d6d52ad932d647a2c7a338263d65cdb022524842
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-04 17:59:01 +0100

    Create oet2_discord.py

---
+++ oet2_discord.py
@@ -0,0 +1,55 @@
+import discord
+import os
+import asyncio
+
+TOKEN = os.getenv("elfak_bot")
+CHANNEL_ID = int(os.getenv("oet2_id"))
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+
+class BOTButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(
+            discord.ui.Button(
+                label="OET 2 link",
+                url="https://eth.elfak.ni.ac.rs/?cat=5",
+                style=discord.ButtonStyle.link
+            )
+        )
+
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+
+    if channel is None:
+        print("Channel not found")
+        await client.close()
+        return
+
+    
+    deleted = 0
+    async for message in channel.history(limit=None):
+        await message.delete()
+        deleted += 1
+        await asyncio.sleep(0.3)
+
+    print(f"Deleted {deleted} messages")
+
+    
+    await channel.send(
+        content="@everyone",
+        view=BOTButton(),
+        file=discord.File("oet2.png")
+    )
+
+    print("New message sent")
+
+    await client.close()
+
+
+client.run(TOKEN)
