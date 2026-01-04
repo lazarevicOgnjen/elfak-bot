b0c33a71df1d695e1be97c14869f93fa2b10d370
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-04 18:26:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,27 +6,4 @@ for url in WEBHOOK_URL:
     hook = Webhook(url)
     hook.send("""@everyone 
 
-Trenutna lista predmeta:
-
-sip - studentski informacioni centar
-
-I godina
-
-uur - uvod u racunarstvo
-aip - algoritmi i programiranje
-
-II godina RI
-
-bp - baze podataka
-oop - objektno orijentisano programiranje
-oopj - objektno orijentisano projektovanje
-pj - programski jezici
-dmat - diskretna matematika
-lp - logicko projektovanje
-sp - strukture podataka
-
-III godina RI
-
-os - operativni sistemi
-rm - racunarske mreze
-ti - teorija igara""")
+Dodati su novi predmeti za I godinu !!!""")
