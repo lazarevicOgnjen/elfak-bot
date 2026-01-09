638e0cfb0afd301f8eae4b17253a95ba23ee1308
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-09 17:26:25 +0100

    Update uur_discord.py

--- uur_discord.py
+++ uur_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@everyone",
+        content="@uur",
         view=BOTButton(),
         file=discord.File("uur.png")
     )
