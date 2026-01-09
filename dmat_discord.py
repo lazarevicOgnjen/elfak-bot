3383c197b9b0022c3c219362f48b415a0e49d5a5
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-09 17:22:32 +0100

    Update dmat_discord.py

--- dmat_discord.py
+++ dmat_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@everyone",
+        content="@dmat",
         view=BOTButton(),
         file=discord.File("dmat.png")
     )
