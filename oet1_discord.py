bb88dd727b42de98778a9f969935df50a7e6f4b1
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-09 17:23:34 +0100

    Change notification from @everyone to @oet1

--- oet1_discord.py
+++ oet1_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@everyone",
+        content="@oet1",
         view=BOTButton(),
         file=discord.File("oet1.png")
     )
