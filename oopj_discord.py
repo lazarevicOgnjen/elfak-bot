e32d9adc8d2713c96bc599c001d0fdad5d93f698
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-09 17:24:50 +0100

    Update oopj_discord.py

--- oopj_discord.py
+++ oopj_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@everyone",
+        content="@oopj",
         view=BOTButton(),
         file=discord.File("oopj.png")
     )
