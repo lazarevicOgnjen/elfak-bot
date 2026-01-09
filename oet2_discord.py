80ab6a78d687dfcc2073f5d51cc4a28871bbfe1f
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-09 17:23:47 +0100

    Change mention from @everyone to @oet2

--- oet2_discord.py
+++ oet2_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@everyone",
+        content="@oet2",
         view=BOTButton(),
         file=discord.File("oet2.png")
     )
