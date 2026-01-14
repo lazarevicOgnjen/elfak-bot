11179a490e40b6c682984a32361d594862d64541
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-15 00:05:27 +0100

    Update sip_discord.py

--- sip_discord.py
+++ sip_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@sip",
+        content="@everyone",
         view=BOTButton(),
         file=discord.File("sip.png")
     )
