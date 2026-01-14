7caf5eb5c505639551985c85c9c4a5b48e39e825
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-15 00:03:51 +0100

    Update oop_discord.py

--- oop_discord.py
+++ oop_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@oop",
+        content="@everyone",
         view=BOTButton(),
         file=discord.File("oop.png")
     )
