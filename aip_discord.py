40c57144adb3912a0deb3bf3b78696157f88b847
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2026-01-15 00:01:32 +0100

    Update aip_discord.py

--- aip_discord.py
+++ aip_discord.py
@@ -42,7 +42,7 @@ async def on_ready():
 
     
     await channel.send(
-        content="@aip",
+        content="@everyone",
         view=BOTButton(),
         file=discord.File("aip.png")
     )
